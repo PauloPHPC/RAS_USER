e088dddea0326e818c33a1a8dc7d3ee853ba9a15
mppc1985 <109829046+mppc1985@users.noreply.github.com>
2023-12-23 11:20:05 +0000

    Add files via upload

--- users_backend/users/models.py
+++ users_backend/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('technician', 'Technician'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'number', 'role']
+
+    def __str__(self):
+        return self.email
--- users_backend/users_backend/urls.py
+++ users_backend/users_backend/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+"""from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+]"""
+
+from django.urls import path
+from .views import create_user
+
+urlpatterns = [
+    path('users/', create_user, name='create_user'),
+    # Aqui você adicionaria mais rotas para outras funcionalidades
 ]
